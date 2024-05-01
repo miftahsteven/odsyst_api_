f490a754fa050d1d8884704af908f96338e2c8eb
Ardha Nur Fajri <ardhanurfajri157@gmail.com>
2024-05-01 23:06:12 +0700

    solving error 0.1

--- app/routes/route-proposal.js
+++ app/routes/route-proposal.js
@@ -6,19 +6,19 @@ const { upload } = require("../helper/upload");
 
 // GET localhost:8080/home => Ambil data semua dari awal
 router.post(
-    "/create",
-    authentication,
-    upload.fields([
-      { name: "lampiran1", maxCount: 1 },
-      { name: "lampiran2", maxCount: 1 },
-      { name: "lampiran3", maxCount: 1 },
-      { name: "lampiran4", maxCount: 1 },
-      { name: "lampiran5", maxCount: 1 },
-      { name: "lampiran6", maxCount: 1 },
-      { name: "lampiran7", maxCount: 1 },
-    ]),
-    proposal.createProposal
-  );
+  "/create",
+  authentication,
+  upload.fields([
+    { name: "lampiran1", maxCount: 1 },
+    { name: "lampiran2", maxCount: 1 },
+    { name: "lampiran3", maxCount: 1 },
+    { name: "lampiran4", maxCount: 1 },
+    { name: "lampiran5", maxCount: 1 },
+    { name: "lampiran6", maxCount: 1 },
+    { name: "lampiran7", maxCount: 1 },
+  ]),
+  proposal.createProposal
+);
 router.get("/details/:id", authentication, proposal.detailProposal);
 router.get("/all", authentication, proposal.getAllProposal);
 router.get("/all-bayar", authentication, proposal.getAllProposalBayar);
@@ -31,5 +31,19 @@ router.put("/done/:id", authentication, proposal.doneProposal);
 router.post("/update/:id", authentication, proposal.updateProposal);
 router.put("/identified/:id", authentication, proposal.updateKategoriPenyaluran);
 //router.post("/upload", authentication, upload.single("lampiran"), proposal.registerProgram);
+router.post(
+  "/create-erp",
+  authentication,
+  upload.fields([
+    { name: "lampiran1", maxCount: 1 },
+    { name: "lampiran2", maxCount: 1 },
+    { name: "lampiran3", maxCount: 1 },
+    { name: "lampiran4", maxCount: 1 },
+    { name: "lampiran5", maxCount: 1 },
+    { name: "lampiran6", maxCount: 1 },
+    { name: "lampiran7", maxCount: 1 },
+  ]),
+  proposal.createProposalErp
+);
 
 module.exports = router;
